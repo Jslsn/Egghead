513ffd5c171917ccc4d2d48067c5c4e29e58d019
Jslsn <jordan.s.layne@gmail.com>
2024-11-04 14:29:01 +0000

    Fixing outtdated code.

--- terraform/deployments/dns/main.tf
+++ terraform/deployments/dns/main.tf
@@ -2,7 +2,7 @@ module "dns" {
   source = "../../modules/route53"
 
   domain                      = var.domain_name
-  bucket_regional_domain_name = data.terraform_remote_state.s3.outputs.bucket_regional_domain_name
-  hosted_zone_id              = data.terraform_remote_state.s3.outputs.bucket_hosted_zone_id
-  bucket_name                 = data.terraform_remote_state.s3.outputs.bucket_name
+  bucket_regional_domain_name = var.bucket_regional_domain_name
+  hosted_zone_id             = var.bucket_hosted_zone_id
+  bucket_name                = var.bucket_name
 } 
--- terraform/deployments/dns/variables.tf
+++ terraform/deployments/dns/variables.tf
@@ -3,12 +3,22 @@ variable "domain_name" {
   description = "Domain name for the website"
 }
 
-variable "state_bucket" {
-  type        = string
-  description = "S3 bucket containing terraform state"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region"
+}
+
+variable "bucket_regional_domain_name" {
+  type        = string
+  description = "The regional domain name of the bucket"
+}
+
+variable "bucket_hosted_zone_id" {
+  type        = string
+  description = "The hosted zone ID for the bucket"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
 } 
