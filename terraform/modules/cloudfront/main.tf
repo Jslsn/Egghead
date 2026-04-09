244f448475b154c0024ad85c0bcdd41d0bf06124
Jslsn <jordan.s.layne@gmail.com>
2026-04-09 20:56:47 +0100

    Fixin cloudfront pt1.

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -3,13 +3,6 @@ resource "aws_cloudfront_distribution" "site_distribution" {
     domain_name = var.s3_endpoint
     origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
     origin_id   = "S3Origin"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
   }
 
   enabled             = true
